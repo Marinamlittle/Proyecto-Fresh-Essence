2a12f2e3d2c26c428f759b134066952d50733223
Marina Mateo <marinaml201101@gmail.com>
2025-05-09 18:16:15 +0200

    Barra de navegacion menos scroll

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,8 +1,3 @@
-/*Scroll hacia abajo*/
-window.addEventListener("scroll", function(){
-    var header = document.querySelector("header");
-    header.classList.toggle("abajo",window.scrollY>0);
-})
 //Barra de navegacion en movil
 const nav = document.querySelector("#nav");
 const abrir = document.querySelector("#abrir");
