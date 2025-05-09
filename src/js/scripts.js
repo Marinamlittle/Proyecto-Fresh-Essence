f9e71e47a2eba438ca24ff8cbff1092847d169d6
Marina Mateo <marinaml201101@gmail.com>
2025-05-09 13:58:48 +0200

    Barra de navegacion

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,3 +3,15 @@ window.addEventListener("scroll", function(){
     var header = document.querySelector("header");
     header.classList.toggle("abajo",window.scrollY>0);
 })
+//Barra de navegacion en movil
+const nav = document.querySelector("#nav");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
+
+abrir.addEventListener("click", () => {
+    nav.classList.add("visible");
+})
+
+cerrar.addEventListener("click", () => {
+    nav.classList.remove("visible");
+})
